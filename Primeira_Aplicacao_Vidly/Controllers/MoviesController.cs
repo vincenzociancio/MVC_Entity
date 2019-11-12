1061cb1716e5c2e1d901f1b9816f1a3aac976714
Vincenzo Moraes - GTINF MTZ <vmoraes@lachmann.com.br>
2019-11-12 10:40:30 -0200

    Adicionar arquivos de projeto.

---
+++ Primeira_Aplicacao_Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Primeira_Aplicacao_Vidly.Models;
+
+namespace Primeira_Aplicacao_Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() {name = "Catch me if you can"};
+
+            //     ViewData["RandomMovie"] = movie;
+            //     ViewBag.RandomMovie = movie;
+
+            var viewResult = new ViewResult();
+
+            return View(movie);
+
+            // return Content("blablablahblabhlabhlabhlabhla");
+
+            // return new EmptyResult();
+
+           // return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+        }
+
+        public ActionResult Edit(int id = 1)
+           {
+            return Content("ID " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "name";
+
+            return Content(string.Format("pageindex= {0}&sortBy= {1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month  )
+        {
+            return Content(year + "/" + month );
+        }
+    }
+}
---
+++ Primeira_Aplicacao_Vidly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Primeira_Aplicacao_Vidly.Startup))]
+namespace Primeira_Aplicacao_Vidly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
